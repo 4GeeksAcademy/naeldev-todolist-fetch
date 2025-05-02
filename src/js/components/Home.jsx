30fd82e022add5fc7d3340ad2132a55054695ab3
Nael <naeldiaz@proton.me>
2025-05-02 17:40:37 +0000

    base

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import Body from "./toDo";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [inputValue, setInputValue] = useState('');
+	const [tasks, setTask] = useState([]);
+	let count =tasks.length;
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const handleChange = (e) => {
+		setInputValue(e.target.value);
+		
+		
+	}
+	const addTask = (e) => {
+		const limite=10;
+		e.preventDefault();
+		if(inputValue.trim()=== ''){
+			alert('Debe contener un valor')
+			return;
+		}
+
+		if(tasks.length < limite){
+			setTask([...tasks, inputValue]);
+			setInputValue('');
+		} else{
+			alert("No puedes tener tantas tareas pendientes, termina o elimina una para agregar otra")
+		}
+		
+		
+	
+	}
+	const deleteTask=(index) =>{
+		const newTasks = tasks.filter((_, i) => i !== index);
+        setTask(newTasks);
+		
+	}
+	
+	
+	
+
+
+	return (
+		<>
+			<div className=" text-center container-fluid m-5 p-2">
+				<div className=" d-flex justify-content-center ">
+					<h1 className="center">To DoS...</h1>
+				</div>
+			</div>
+			<div className="post-it">
+				<Body
+					handleChange={handleChange}
+					value={inputValue}
+					addTask={addTask}
+				/>
+				<div className="mt-3 text-center">
+					{tasks.map((task, index) => (
+						
+						<div key={index} className="text-center p-2 d-flex align-items-center">
+						<input 
+							className="form-check-input me-2" 
+							type="checkbox" 
+							value="" 
+						/>
+						<span className="flex-grow-1 text-center">
+							{task}
+						</span>
+						<button
+							onClick={() => deleteTask(index)}
+							type="button"
+							className="btn boton">
+							x
+						</button>
+					</div>
+
+					))}
+
+
+
+				</div>
+				<div>
+					{count === 0 ? "No hay tareas pendientes, añadir tareas" : `${count} tareas pendientes`}
+
+				</div>
+			</div>
+
+
+
+
+		</>
+	)
+
 };
 
 export default Home;
---
+++ src/js/components/toDo.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Body = ({handleChange, inputValue,addTask}) => {
+    return (
+        <div className="container-fluid text-center ">
+            <form onSubmit={addTask}>
+            
+                <input 
+                    type="text"
+                    value={inputValue} 
+                    placeholder="Ingresa la tarea"
+                    onChange={handleChange}
+                    className="form-control me-2"
+                    maxLength={120}
+                />
+        
+                
+
+            </form>
+            </div>
+
+
+        
+    )
+}
+
+export default Body
